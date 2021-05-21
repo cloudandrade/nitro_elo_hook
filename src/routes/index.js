bc53c2b86ae1c8fe1079fd28dc4ce29bbdda209f
Jan Andrade <cloud.andrade16@gmail.com>
2021-05-21 08:58:47 -0300

    feat: validando e tratando erros

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const cheerio = require('cheerio');
 const Logger = require('../services/logger_service');
 const routes = Router();
 
-const logger = new Logger('requisitions');
+const logger = new Logger('server');
 //================================= SERVER
 //NEH - Nitro Elo Hook
 
@@ -24,16 +24,19 @@ routes.get('/', async (req, res) => {
 routes.get('/elos', async (req, res) => {
 	let { summoner, contractor, contractorHash } = req.query;
 
+
+	if (summoner) {
+		logger.debug(
+			`Foi requisitado elo do jogador: ${summoner} em League of Legends`
+		);
+	}
+	if (contractor) {
+		logger.debug(`Foi requisitado elo do jogador: ${contractor} em Valorant`);
+	}
+	
 	let response = {};
 	await getElos(summoner, contractor, contractorHash).then((elos) => {
-		if (summoner) {
-			logger.debug(
-				`Foi requisitado elo do jogador: ${summoner} em League of Legends`
-			);
-		}
-		if (contractor) {
-			logger.debug(`Foi requisitado elo do jogador: ${contractor} em Valorant`);
-		}
+		
 		response = elos;
 		res.send(response);
 	});
@@ -46,10 +49,11 @@ async function getElos(summonerName, contractor, contractorHash) {
 	//se o invocador for passado por parametro ele irá buscar o elo
 	if (summonerName) {
 		let game1 = {};
-		try {
+	
 			await axios
 				.get(`https://www.leagueofgraphs.com/pt/summoner/br/${summonerName}`)
 				.then((res) => {
+					console.log(res.status);
 					const $ = cheerio.load(res.data);
 					const pickLolElo = $('.leagueTier').html();
 
@@ -57,18 +61,37 @@ async function getElos(summonerName, contractor, contractorHash) {
 					game1.summoner = summonerName;
 					game1.elo = pickLolElo.trim();
 					elos.game1 = game1;
+				}).catch(error => {
+					switch (error.response.status) {
+						case 404:
+							logger.info("Player não encotrado. ".yellow + error.response.statusText);
+							game1.erro = "Player não encotrado";
+							elos.game1 = game1;
+							break;
+						case 500:
+							logger.error("Erro desconhecido no serviço de busca. " + error.response.statusText);
+							game1.erro = "Erro desconhecido no serviço de busca";
+							elos.game1 = game1;
+							break;
+						case 401:
+							logger.error("Busca não autorizada, usuário privado. ".yellow + error.response.statusText);
+							game1.erro = "Erro desconhecido no serviço de busca";
+							elos.game1 = game1;
+							break;
+						default:
+							logger.error("Erro não tratado. " + error);
+							game1.erro = "Erro desconhecido no serviço gateway";
+							elos.game1 = game1;
+							break;
+					}
 				});
-		} catch (error) {
-			logger.error(
-				'Falha ao buscar elo de invocador em league of legends - ' + error
-			);
-		}
+		
 	}
 
 	//se o contratante, ou seja, o jogador de valorant for passado, ele irá buscar o elo
 	if (contractor && contractorHash) {
 		let game2 = {};
-		try {
+		
 			await axios
 				.get(
 					`https://tracker.gg/valorant/profile/riot/${contractor}%23${contractorHash}/overview?playlist=competitive`
@@ -82,10 +105,31 @@ async function getElos(summonerName, contractor, contractorHash) {
 					game2.summoner = summonerName;
 					game2.elo = pickValoElo.trim();
 					elos.game2 = game2;
+				}).catch(error => {
+					switch (error.response.status) {
+						case 404:
+							logger.info("Player não encotrado. ".yellow + error.response.statusText);
+							game2.erro = "Player não encotrado";
+							elos.game2 = game2;
+							break;
+						case 500:
+							logger.error("Erro desconhecido no serviço de busca. " + error.response.statusText);
+							game2.erro = "Erro desconhecido no serviço de busca";
+							elos.game2 = game2;
+							break;
+						case 401:
+							logger.error("Busca não autorizada, usuário privado. ".yellow + error.response.statusText);
+							game2.erro = "Erro desconhecido no serviço de busca";
+							elos.game2 = game2;
+							break;
+						default:
+							logger.error("Erro não tratado. " + error);
+							game2.erro = "Erro desconhecido no serviço gateway";
+							elos.game2 = game2;
+							break;
+					}
 				});
-		} catch (error) {
-			logger.error('Falha ao buscar elo de contratante em valorant - ' + error);
-		}
+	
 	}
 
 	return elos;
